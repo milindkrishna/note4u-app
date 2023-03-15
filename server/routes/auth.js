5b4736964fe21065ad7e4331f5e0fb464fc078ab
Milind Krishna <54195987+milindkrishna@users.noreply.github.com>
2023-03-15 10:14:50 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -10,7 +10,8 @@ const User = require('../models/User')
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    proxy: true
   },
 
   async function(accessToken, refreshToken, profile, done) {
